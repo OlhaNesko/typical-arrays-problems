5c47d4845ed581877b8d8a6631946aafd6eb05a0
OlhaNesko <64435016+OlhaNesko@users.noreply.github.com>
2020-10-01 17:48:45 +0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
+exports.min = function min(array) {
+    if (array === undefined || array[0] === undefined) return 0;
+    return Math.min(...array);
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (array === undefined || array[0] === undefined) return 0;
+    return Math.max(...array);
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (array === undefined || array[0] === undefined) return 0;
+    for (var i = 0, sum = 0; i < array.length; i++) {
+        sum += array[i];
+    }
+    return sum / i;
+};
